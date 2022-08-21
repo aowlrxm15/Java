9e3673b6695bc9ea58533da991747adf12181563
aowlrxm15 <aowlrxm15@gmail.com>
2022-08-21 19:18:16 +0900

    Backjoon/ 3 - 04추 가

--- Backjoon/src/step02/_06.java
+++ Backjoon/src/step02/_06.java
@@ -1,10 +1,24 @@
 package step02;
 
+import java.util.Scanner;
+
 public class _06 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+ 
+        int A = in.nextInt();
+        int B = in.nextInt();
+ 
+        int C = in.nextInt();
+ 
+        int min = 60 * A + B;   // 시 -> 분
+        min += C;
+ 
+        int hour = (min / 60) % 24;
+        int minute = min % 60;
+ 
+        System.out.println(hour + " " + minute);
+ 
+    }
 }
