1cfec8e8ecb18b7da2cbbe698f2b7701e620a285
aowlrxm15 <aowlrxm15@gmail.com>
2022-08-23 22:52:41 +0900

    Exam/끝

--- Exam/src/sub01/Test04.java
+++ Exam/src/sub01/Test04.java
@@ -6,7 +6,7 @@ public class Test04 {
 
 	public static void main(String[] args) {
 		
-		Scanner se = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("점수 입력 :");
 		int score = sc.nextInt();
@@ -15,15 +15,23 @@ public class Test04 {
 		//grade
 		
 		if(score >= 90 && score <=100) {
-			
-		}// 80 90 순서대로내려감
+			grade = 'A';
+		}else if (score >= 80 && score <=90) {
+			grade = 'B';
+		}else if (score >= 70 && score <=80) {
+			grade = 'C';
+		}else if (score >= 60 && score <=70) {
+			grade = 'D';
+		}else {
+			grade = 'F';
+		}
+		
+		System.out.printf("등급은 %c입니다." , grade);
 		
 		
 		
 		
-		
-		
-
+		// 80 90 순서대로내려감
 	}
 
 }
--- Exam/src/sub01/Test08.java
+++ Exam/src/sub01/Test08.java
@@ -3,8 +3,24 @@ package sub01;
 public class Test08 {
 
 	public static void main(String[] args) {
-		// j > i 
-		// j < i * 2 + 1 or 1빼고
+		
+		int n = 5;
+		
+		for (int i = 0 ; i<n ; i++) {
+			
+			for(int j=n-1 ; j > i ; j--) {
+				System.out.println(" ");
+			}
+			
+			for(int j=0 ; j < i * 2 + 1 ; j++) {
+				System.out.println("*");
+			}
+		
+			System.out.println("\n"); 
+			// j > i 
+			// j < i * 2 + 1 
+		
+		}
 
 	}
 
