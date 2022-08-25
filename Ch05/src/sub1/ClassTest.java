ee955e95c99e6502d369be38b7fc18b20afbab38
aowlrxm15 <aowlrxm15@gmail.com>
2022-08-25 16:37:46 +0900

    수정

--- Ch05/src/sub1/ClassTest.java
+++ Ch05/src/sub1/ClassTest.java
@@ -3,6 +3,10 @@ package sub1;
  * 날짜 : 2022/08/25
  * 이름 : 전인준
  * 내용 : Java 클래스 실습하기
+ * 
+ * 클래스와 객체
+ * - 클래스는 객체를 생성하는 구조체이고 속성(필드)과 기능(메서드)으로 구성
+ * - 객체는 클래스의 실제 인스터스이고 new 연산으로 생성
  */
 public class ClassTest {
 	public static void main(String[] args) {
@@ -48,6 +52,17 @@ public class ClassTest {
 		kb.deposit(50000);
 		kb.withdraw(7000);
 		kb.show(); 
+		
+		Account wr = new Account();
+		
+		wr.bank = "우리은행";
+		wr.id = "101-13-1002";
+		wr.name = "장보고";
+		wr.balance = 10000;
+		
+		wr.deposit(50000);
+		wr.withdraw(20000);
+		wr.show(); 
 		
 		
 
